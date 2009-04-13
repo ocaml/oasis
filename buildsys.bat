75f3864316d8c2af2d3b4c76b8d33ce020d74e70
sylvain.le-gall <sylvain.le-gall@polytechnique.org>
2009-04-13 22:32:51 +0100

    Temporary deactivate bootstrap and remove all files that permit it
    Ignore-this: 1b2771046422e97355694f02a200212b
    
    buildsys.ml has been split into several file. Don't use it anymore and remove
    it. Bootstrapping the build process will be reactivated when possible.
    
    darcs-hash:20090413213251-4d692-626e8f33ad2df0b35a56ca251844de47e99b8d92

--- buildsys.bat
+++
@@ -1,5 +0,0 @@
-rem+ AUTOBUILD_START 
-rem+ DO NOT EDIT THIS PART 
-rem+ tag content_digest "c4a96d682d2346147fc43996fb4dc5f4" 
-rem TODO
-rem+ AUTOBUILD_STOP 
--- src/base/buildsys.sh
+++
@@ -1,4 +0,0 @@
-
-OCAMLFLAGS=$(ocamlfind query -format "-I %d %a" -separator " " -suffix "" -r -predicates byte,toploop $PACKAGES)
-
-exec ocaml $OCAMLFLAGS $(dirname $0)/buildsys.ml $*
