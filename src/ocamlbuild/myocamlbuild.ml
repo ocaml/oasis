9a023f2518183e5f8dbdb921453c368a1c1db1d8
sylvain.le-gall <sylvain.le-gall@polytechnique.org>
2009-04-20 16:15:26 +0100

    Update and add AUTOBUILD_START/STOP to myocamlbuild.ml
    darcs-hash:20090420151526-4d692-c80751cbcc7c1fe024fea316a97370716b9527f3

--- src/ocamlbuild/myocamlbuild.ml
+++ src/ocamlbuild/myocamlbuild.ml
@@ -1,11 +1,7 @@
-(** OCamlbuild extension, copied from Nicolas Pouillard mail to caml list
-  * http://caml.inria.fr/pub/ml-archives/caml-list/2008/03/7e686d65c788f52003e60c9b20d245ff.en.html
-  *
-  * Modified by Sylvain Le Gall 
-  *)
 
-open Ocamlbuild_plugin
+(* AUTOBUILD_START *)
 
+(*
 (* Win32/Unix env *)
 let () = 
   match Sys.os_type with 
@@ -13,82 +9,110 @@ let () =
         Options.ext_obj := "obj"
     | _ ->
         ()
-
-(* These functions are not really officially exported *)
-let run_and_read = 
-  Ocamlbuild_pack.My_unix.run_and_read
-
-let blank_sep_strings = 
-  Ocamlbuild_pack.Lexers.blank_sep_strings
-
-(* This lists all supported packages *)
-let find_packages () =
-  blank_sep_strings &
-    Lexing.from_string &
-    run_and_read "ocamlfind list | cut -d' ' -f1"
-
-(* This is supposed to list available syntaxes, but I don't know how to do it.
  *)
-let find_syntaxes () = 
-  ["camlp4o"; "camlp4r"]
+module OCamlfind =
+struct
+  (** OCamlbuild extension, copied from 
+    * http://brion.inria.fr/gallium/index.php/Using_ocamlfind_with_ocamlbuild
+    * by N. Pouillard and others
+    *
+    * Modified by Sylvain Le Gall 
+    *)
+  open Ocamlbuild_plugin
 
-(* ocamlfind command *)
-let ocamlfind x = 
-  S[A"ocamlfind"; x]
+  (* these functions are not really officially exported *)
+  let run_and_read = 
+    Ocamlbuild_pack.My_unix.run_and_read
 
-let dispatch_ocamlfind = 
-  function
-    | Before_options ->
-        (* override default commands by ocamlfind ones *)
-        Options.ocamlc   := ocamlfind & A"ocamlc";
-        Options.ocamlopt := ocamlfind & A"ocamlopt";
-        Options.ocamldep := ocamlfind & A"ocamldep";
-        Options.ocamldoc := ocamlfind & A"ocamldoc"
-                              
-    | After_rules ->
-        (* When one link an OCaml library/binary/package, one should use 
-         * -linkpkg
-         *)
-        flag ["ocaml"; "link"] & A"-linkpkg";
-              
-        (* For each ocamlfind package one inject the -package option when 
-         * compiling, computing dependencies, generating documentation and 
-         * linking.
-         *)
-         List.iter 
-          begin fun pkg ->
-            flag ["ocaml"; "compile";  "pkg_"^pkg] & S[A"-package"; A pkg];
-            flag ["ocaml"; "ocamldep"; "pkg_"^pkg] & S[A"-package"; A pkg];
-            flag ["ocaml"; "doc";      "pkg_"^pkg] & S[A"-package"; A pkg];
-            flag ["ocaml"; "link";     "pkg_"^pkg] & S[A"-package"; A pkg];
-          end 
-          (find_packages ());
-        (* Like -package but for extensions syntax. Morover -syntax is useless 
-         * when linking. 
-         *)
-         List.iter 
-          begin fun syntax ->
-            flag ["ocaml"; "compile";  "syntax_"^syntax] & S[A"-syntax"; A syntax];
-            flag ["ocaml"; "ocamldep"; "syntax_"^syntax] & S[A"-syntax"; A syntax];
-            flag ["ocaml"; "doc";      "syntax_"^syntax] & S[A"-syntax"; A syntax];
-          end 
-          (find_syntaxes ());
+  let blank_sep_strings = 
+    Ocamlbuild_pack.Lexers.blank_sep_strings
 
-        (* The default "thread" tag is not compatible with ocamlfind. Indeed, the
-         * default rules add the "threads.cma" or "threads.cmxa" options when
-         * using this tag. When using the "-linkpkg" option with ocamlfind, this
-         * module will then be added twice on the command line.  To solve this,
-         * one approach is to add the "-thread" option when using the "threads"
-         * package using the previous plugin. 
-         *)
-         flag ["ocaml"; "pkg_threads"; "compile"] & (S[A"-thread"]);
-         flag ["ocaml"; "pkg_threads"; "link"]    & (S[A"-thread"]);
+  let split s ch =
+    let x = 
+      ref [] 
+    in
+    let rec go s =
+      let pos = 
+        String.index s ch 
+      in
+        x := (String.before s pos)::!x;
+        go (String.after s (pos + 1))
+    in
+      try
+        go s
+      with Not_found -> !x
 
-         flag ["ocaml"; "pp_camlp4o"; "compile"]  & (S[A"-pp"; A"camlp4o"]);
-         flag ["ocaml"; "pp_camlp4o"; "ocamldep"] & (S[A"-pp"; A"camlp4o"]);
-         flag ["ocaml"; "pp_camlp4o"; "doc"]      & (S[A"-pp"; A"camlp4o"]);
+  let split_nl s = split s '\n'
 
-    | _ -> ()
-;;
+  let before_space s =
+    try
+      String.before s (String.index s ' ')
+    with Not_found -> s
 
-dispatch dispatch_ocamlfind;;
+  (* this lists all supported packages *)
+  let find_packages () =
+    List.map before_space (split_nl & run_and_read "ocamlfind list")
+
+  (* this is supposed to list available syntaxes, but I don't know how to do it. *)
+  let find_syntaxes () = ["camlp4o"; "camlp4r"]
+
+  (* ocamlfind command *)
+  let ocamlfind x = S[A"ocamlfind"; x]
+
+  let dispatch =
+    function
+      | Before_options ->
+          (* by using Before_options one let command line options have an higher priority *)
+          (* on the contrary using After_options will guarantee to have the higher priority *)
+          (* override default commands by ocamlfind ones *)
+          Options.ocamlc     := ocamlfind & A"ocamlc";
+          Options.ocamlopt   := ocamlfind & A"ocamlopt";
+          Options.ocamldep   := ocamlfind & A"ocamldep";
+          Options.ocamldoc   := ocamlfind & A"ocamldoc";
+          Options.ocamlmktop := ocamlfind & A"ocamlmktop"
+                                  
+      | After_rules ->
+          
+          (* When one link an OCaml library/binary/package, one should use -linkpkg *)
+          flag ["ocaml"; "link"] & A"-linkpkg";
+          
+          (* For each ocamlfind package one inject the -package option when
+           * compiling, computing dependencies, generating documentation and
+           * linking. *)
+          List.iter 
+            begin fun pkg ->
+              flag ["ocaml"; "compile";  "pkg_"^pkg] & S[A"-package"; A pkg];
+              flag ["ocaml"; "ocamldep"; "pkg_"^pkg] & S[A"-package"; A pkg];
+              flag ["ocaml"; "doc";      "pkg_"^pkg] & S[A"-package"; A pkg];
+              flag ["ocaml"; "link";     "pkg_"^pkg] & S[A"-package"; A pkg];
+              flag ["ocaml"; "infer_interface"; "pkg_"^pkg] & S[A"-package"; A pkg];
+            end 
+            (find_packages ());
+
+          (* Like -package but for extensions syntax. Morover -syntax is useless
+           * when linking. *)
+          List.iter begin fun syntax ->
+          flag ["ocaml"; "compile";  "syntax_"^syntax] & S[A"-syntax"; A syntax];
+          flag ["ocaml"; "ocamldep"; "syntax_"^syntax] & S[A"-syntax"; A syntax];
+          flag ["ocaml"; "doc";      "syntax_"^syntax] & S[A"-syntax"; A syntax];
+          flag ["ocaml"; "infer_interface"; "syntax_"^syntax] & S[A"-syntax"; A syntax];
+          end (find_syntaxes ());
+
+          (* The default "thread" tag is not compatible with ocamlfind.
+           * Indeed, the default rules add the "threads.cma" or "threads.cmxa"
+           * options when using this tag. When using the "-linkpkg" option with
+           * ocamlfind, this module will then be added twice on the command line.
+           *                        
+           * To solve this, one approach is to add the "-thread" option when using
+           * the "threads" package using the previous plugin.
+           *)
+          flag ["ocaml"; "pkg_threads"; "compile"] (S[A "-thread"]);
+          flag ["ocaml"; "pkg_threads"; "link"] (S[A "-thread"]);
+          flag ["ocaml"; "pkg_threads"; "infer_interface"] (S[A "-thread"])
+
+      | _ -> 
+          ()
+end;;
+(* AUTOBUILD_STOP *)
+
+Ocamlbuild_plugin.dispatch OCamlfind.dispatch;;
