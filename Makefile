843834dc2593d1ed3a1d1db94364ba888ab7950e
Sylvain Le Gall <sylvain.le-gall@ocamlcore.com>
2010-04-01 00:30:29 +0100

    Add standard documentation and header
    Ignore-this: 4f2b8f2860dba2efffb403ce08d8e439
    
    darcs-hash:20100331233029-a43a9-5787193f8818d8d45b3bf8ee8a9d98af2d03743d

--- Makefile
+++ Makefile
@@ -1,3 +1,25 @@
+################################################################################
+#  OASIS: architecture for building OCaml libraries and applications           #
+#                                                                              #
+#  Copyright (C) 2008-2010, OCamlCore SARL                                     #
+#                                                                              #
+#  This library is free software; you can redistribute it and/or modify it     #
+#  under the terms of the GNU Lesser General Public License as published by    #
+#  the Free Software Foundation; either version 2.1 of the License, or (at     #
+#  your option) any later version, with the OCaml static compilation           #
+#  exception.                                                                  #
+#                                                                              #
+#  This library is distributed in the hope that it will be useful, but         #
+#  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY  #
+#  or FITNESS FOR A PARTICULAR PURPOSE. See the file COPYING for more          #
+#  details.                                                                    #
+#                                                                              #
+#  You should have received a copy of the GNU Lesser General Public License    #
+#  along with this library; if not, write to the Free Software Foundation,     #
+#  Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA               #
+################################################################################
+
+default: TESTFLAGS=-long
 default: test
 
 HAS_GETTEXT=$(shell if ocamlfind query gettext > /dev/null 2>&1; then \
@@ -34,3 +56,7 @@ wc:
 doc: all
 	env LANG=C _build/src/OASIS.byte -documentation > doc/MANUAL.mkd
 
+headache:
+	find ./ -name _darcs -prune -false -o -name _build -prune \
+	  -o -name ext -prune -false -o -type f \
+	  | xargs headache -h _header -c _headache.config
--- src/tools/ocamlmod.ml
+++ src/tools/ocamlmod.ml
@@ -1,3 +1,23 @@
+(********************************************************************************)
+(*  OASIS: architecture for building OCaml libraries and applications           *)
+(*                                                                              *)
+(*  Copyright (C) 2008-2010, OCamlCore SARL                                     *)
+(*                                                                              *)
+(*  This library is free software; you can redistribute it and/or modify it     *)
+(*  under the terms of the GNU Lesser General Public License as published by    *)
+(*  the Free Software Foundation; either version 2.1 of the License, or (at     *)
+(*  your option) any later version, with the OCaml static compilation           *)
+(*  exception.                                                                  *)
+(*                                                                              *)
+(*  This library is distributed in the hope that it will be useful, but         *)
+(*  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY  *)
+(*  or FITNESS FOR A PARTICULAR PURPOSE. See the file COPYING for more          *)
+(*  details.                                                                    *)
+(*                                                                              *)
+(*  You should have received a copy of the GNU Lesser General Public License    *)
+(*  along with this library; if not, write to the Free Software Foundation,     *)
+(*  Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA               *)
+(********************************************************************************)
 
 (** Tool to transform a source module into an OCaml module
     @author Sylvain Le Gall
@@ -49,8 +69,14 @@ let dump_ml chn_out fn =
     let export_start = 
       Str.regexp "(\\* +START +EXPORT +\\*)"
     in
+    let beg_comment = 
+      Str.regexp "(\\*.*" 
+    in
+    let pass_beg_comment =
+      ref false
+    in
     let export =
-      ref true
+      ref false
     in
     let line_num = 
       ref 0
@@ -66,6 +92,13 @@ let dump_ml chn_out fn =
               incr line_num;
               input_line chn_in
             in
+              if not !pass_beg_comment 
+              && not (Str.string_match beg_comment line 0) then
+                begin
+                  pass_beg_comment := true;
+                  export := true
+                end;
+
               if Str.string_match export_end line 0 then
                 begin
                   export := false;
