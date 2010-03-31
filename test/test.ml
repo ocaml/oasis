d0a48778e464f16347f9875d8355d719528957a3
Sylvain Le Gall <sylvain.le-gall@ocamlcore.com>
2010-03-31 23:07:58 +0100

    Give access to test settings on command line
    Ignore-this: 5622624d6591775719fd014dc51b8cc3
    
    darcs-hash:20100331220758-a43a9-688017ad737c8c9d53462f27a6aca88e0a2d74a1

--- test/test.ml
+++ test/test.ml
@@ -8,10 +8,39 @@ open TestCommon;;
 open OASISBuiltinPlugins;;
 
 let _res: test_result list = 
+  let dbug =
+    ref false
+  in
+  let long =
+    ref false
+  in
+  let only_tests =
+    ref []
+  in
+
+  let () = 
+    Arg.parse
+      [
+        "-verbose",
+        Arg.Set dbug,
+        "Run the test in verbose mode.";
+
+        "-only-test",
+        Arg.String (fun s -> only_tests := s :: !only_tests),
+        "Run only the selected test";
+
+        "-long",
+        Arg.Set long,
+        "Run long tests";
+      ]
+      invalid_arg
+      ("usage: "^Sys.executable_name^" [options*]")
+  in
+
   let ctxt =
     {
-      dbug = false;
-      long = false;
+      dbug = !dbug;
+      long = !long;
       has_ocamlopt = 
         (
           try 
@@ -29,7 +58,15 @@ let _res: test_result list =
       oasis_args = [];
     }
   in
-    run_test_tt_main
+
+  let tests = 
+    (if !only_tests = [] then
+       (fun l -> l)
+     else
+       (fun l ->
+          match test_filter !only_tests l with
+            | Some l -> l
+            | None -> failwith "No tests to execute"))
       ("OASIS">:::
        [
          TestPropList.tests     ctxt;
@@ -41,4 +78,7 @@ let _res: test_result list =
          TestMETA.tests         ctxt;
          TestLog.tests          ctxt;
        ])
+  in
+
+    run_test_tt ~verbose:!dbug tests
 ;;
