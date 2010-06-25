d3bb824e64cf7b833fb13c9e233e970f33c27339
Sylvain Le Gall <sylvain.le-gall@ocamlcore.com>
2010-06-25 14:49:10 +0100

    Add an option to setup-clean to empty replace section of generated files (Closes: #564)
    Ignore-this: f85506c7c04c3caf87d337628f6ca7ec
    
    darcs-hash:20100625134910-a43a9-cc252955744a817eeaaa35fa093a79eee78b6ba1

--- src/cli/SetupClean.ml
+++ src/cli/SetupClean.ml
@@ -5,16 +5,54 @@
 
 open MainGettext
 open SubCommand
+open OASISFileTemplate
+open OASISPlugin 
+
+let replace_sections =
+  ref false
 
 let main () = 
-  BaseGenerate.restore ()
+  BaseGenerate.restore ();
+  if !replace_sections then
+    begin
+      let ctxt, _ = 
+        BaseSetup.of_package 
+          (OASIS.from_file !Setup.oasis_fn)
+      in
+        OASISFileTemplate.fold
+          (fun tmpl () ->
+             match tmpl.body with 
+             | Body _ 
+             | BodyWithDigest _ ->
+                 begin
+                   let _chng: file_generate_change =
+                     file_generate 
+                       ~backup:false 
+                       {tmpl with body = Body []}
+                   in
+                     ()
+                 end
+             | NoBody ->
+                 ())
+          ctxt.files
+          ()
+    end
 
 let scmd = 
-  SubCommand.make
-    "setup-clean"
-    (s_ "Clean all template files from their content")
-    CLIData.setup_clean_mkd
-    main
+  {(SubCommand.make
+      ~std_usage:true
+      "setup-clean"
+      (s_ "Clean all template files from their content")
+      CLIData.setup_clean_mkd
+      main)
+     with 
+         scmd_specs =
+           [
+             "-replace-sections",
+             Arg.Set replace_sections,
+             s_ "Empty replace section in generated files (i.e. remove content \
+                 between OASIS_START and OASIS_STOP).";
+           ]}
 
 let () = 
   SubCommand.register scmd
