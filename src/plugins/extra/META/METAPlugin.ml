044ffbeb82307052252d232a117627108e049498
Sylvain Le Gall <sylvain.le-gall@ocamlcore.com>
2010-09-10 18:19:45 +0100

    Add an exists_if field to generated META file (Closes: #694)
    Ignore-this: df5410eedc227b1e2a8cdf45c7df765
    
    darcs-hash:20100910171945-a43a9-7a806715346ebad44ce1ecdec71d827e8f952736

--- src/plugins/extra/META/METAPlugin.ml
+++ src/plugins/extra/META/METAPlugin.ml
@@ -112,6 +112,9 @@ let pp_print_meta pkg findlib_name_map fmt grp =
     let lib_name =
       lib_cs.cs_name
     in
+    let lib_cma, lib_cmxa = 
+      lib_name^".cma", lib_name^".cmxa"
+    in
       pp_print_sfield fmt ("version", (OASISVersion.string_of_version pkg.version));
       begin
         let txt =
@@ -144,19 +147,25 @@ let pp_print_meta pkg findlib_name_map fmt grp =
       begin
         match typ lib_cs.cs_data with 
           | METALibrary ->
-              pp_print_field fmt ("archive", ["byte"], lib_name^".cma");
+              pp_print_field fmt ("archive", ["byte"], lib_cma);
               begin
                 match lib_bs.bs_compiled_object with
                   | Best | Native ->
-                      pp_print_field fmt ("archive", ["native"], lib_name^".cmxa");
+                      pp_print_field fmt ("archive", ["native"], lib_cmxa);
                   | Byte ->
                       ()
               end
 
           | METASyntax ->
-              pp_print_field fmt ("archive", ["syntax"; "preprocessor"], lib_name^".cma");
-              pp_print_field fmt ("archive", ["syntax"; "toploop"], lib_name^".cma")
+              pp_print_field fmt ("archive", ["syntax"; "preprocessor"], lib_cma);
+              pp_print_field fmt ("archive", ["syntax"; "toploop"], lib_cma)
       end;
+      pp_print_sfield fmt 
+        ("exists_if", 
+         if lib_bs.bs_compiled_object = Native then 
+           lib_cmxa
+         else
+           lib_cma);
       FormatExt.pp_print_list pp_print_group "@," fmt children
 
   and pp_print_group fmt = 
