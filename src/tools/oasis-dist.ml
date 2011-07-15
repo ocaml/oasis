c6118f91a43310a00855a786866016a562b0b60f
Sylvain Le Gall <sylvain.le-gall@ocamlcore.com>
2011-07-15 14:02:03 +0100

    Add git support to oasis-dist
    Ignore-this: 5dff19ca30d6fffc17db0d5c50a7eb4d
    
    darcs-hash:20110715130203-a43a9-68f6b3c7c02b733b966f0203814201e7d1986368

--- src/tools/oasis-dist.ml
+++ src/tools/oasis-dist.ml
@@ -8,6 +8,7 @@
 
 open OASISMessage
 open OASISTypes
+open OASISUtils
 open FileUtil
 
 let with_tmpdir f = 
@@ -15,7 +16,11 @@ let with_tmpdir f =
     Filename.temp_file "oasis-dist-" ".dir"
   in
   let clean () = 
-    rm ~recurse:true [res]
+(*     rm ~recurse:true [res] *)
+    let _i : int = 
+      Sys.command (Printf.sprintf "rm -rf %s" (Filename.quote res))
+    in
+      ()
   in
     Sys.remove res; 
     FileUtil.mkdir res; 
@@ -26,22 +31,51 @@ let with_tmpdir f =
       clean ();
       raise e
 
-type contxt = OASISContext.t
+let update_oasis_in_tarball fn topdir = 
+  with_tmpdir
+    (fun dn ->
+       BaseExec.run "tar" ["-C"; dn; "-xzf"; fn];
+       BaseExec.run "oasis" ["-C"; Filename.concat dn topdir; "setup"];
+       BaseExec.run "tar" ["-C"; dn; "-czf"; fn; topdir])
 
 class virtual vcs = 
 object
-  method check_dist = 
-    true
-
   method check_uncommited_changes = 
     true
 
   method check_tag (ver: string) =
     true
 
-  method virtual dist : host_filename -> unit
+  method virtual dist : string -> host_filename -> unit
 
   method virtual tag : string -> unit
+end
+
+class svn ~ctxt =
+object 
+  inherit vcs
+
+  method check_uncommited_changes =
+    match BaseExec.run_read_output "svn" ["status"] with
+      | [] ->
+          true
+      | lst ->
+          false
+
+  method dist topdir tarball = 
+    with_tmpdir 
+      (fun dir ->
+         let tgt = 
+           Filename.concat dir topdir
+         in
+         let cur_pwd = 
+           pwd ()
+         in
+           BaseExec.run "svn" ["export"; cur_pwd; tgt];
+           BaseExec.run "tar" ["-C"; dir; "-czf"; tarball; topdir])
+
+  method tag ver =
+    warning ~ctxt "No tag method"
 end
 
 class darcs ~ctxt = 
@@ -49,35 +83,6 @@ object
   inherit vcs
 
   val ctxt = ctxt
-
-  method check_dist = 
-    (* Check that predist contains "OASIS setup" *)
-    let predist_ok =
-      ref false
-    in
-    let () = 
-      try 
-        let chn = 
-          open_in (Filename.concat "_darcs" (Filename.concat "prefs" "prefs"))
-        in
-          Pcre.foreach_line 
-            ~ic:chn
-            (fun line ->
-               if Pcre.pmatch ~pat:"^predist.*\\boasis\\b.*\\bsetup\\b.*$" line then
-                 predist_ok := true);
-      with _ ->
-        ()
-    in
-      if not !predist_ok then
-        begin
-          error ~ctxt 
-            "predist doesn't contain 'oasis setup' invocation";
-          error ~ctxt
-            "Run 'darcs setpref predist \"oasis setup\"'";
-          false
-        end
-      else
-        true
 
   method check_uncommited_changes = 
     let ok = 
@@ -90,9 +95,10 @@ object
            | 1 -> 
                ok := true
            | 0 ->
-               error ~ctxt "Uncommited changes"
+               ()
            | n ->
-               error ~ctxt "Unexpected exit code %d" n
+               failwithf
+                 "Unexpected exit code %d" n
         )
         "darcs" ["whatsnew"; "-ls"];
       !ok
@@ -103,12 +109,44 @@ object
       in
         not (List.mem ver out)
 
-  method dist tarname = 
+  method dist topdir tarball = 
     (* Create the tarball *)
-    BaseExec.run "darcs" ["dist"; "--dist-name"; tarname]
+    BaseExec.run "darcs" ["dist"; "--dist-name"; topdir];
+    mv (topdir^".tar.gz") tarball
 
   method tag ver = 
     BaseExec.run "darcs" ["tag"; ver]
+end
+
+class git ~ctxt =
+object
+  inherit vcs
+
+  method check_uncommited_changes = 
+    match BaseExec.run_read_output "git" ["status"; "--porcelain"] with 
+      | [] -> 
+          true
+      | _ ->
+          false
+
+  method check_tag ver = 
+    let out =
+      BaseExec.run_read_output "git" ["tag"]
+    in
+      not (List.mem ver out)
+
+  method dist topdir tarball = 
+    let tarfn = 
+      Filename.chop_extension tarball
+    in
+      BaseExec.run 
+        "git" 
+        ["archive"; "--prefix";  (Filename.concat topdir "");
+         "--format"; "tar"; "HEAD"; "-o"; tarfn];
+      BaseExec.run "gzip" [tarfn]
+
+  method tag ver =
+    BaseExec.run "git" ["tag"; ver]
 end
 
 class no_vcs ~ctxt =
@@ -117,35 +155,27 @@ object
 
   val ctxt = ctxt
 
-  method dist tarname = 
+  method dist topdir tarball = 
     with_tmpdir 
       (fun dir ->
          let tgt = 
-           Filename.concat dir tarname
+           Filename.concat dir topdir
          in
          let cur_pwd = 
            pwd ()
          in
-         let tarball =
-           Filename.concat cur_pwd (tarname^".tar.gz")
-         in
-(*
-           cp ~recurse:true [cur_pwd] tgt;
- *)
            BaseExec.run "cp" ["-r"; cur_pwd; tgt];
            begin
              try 
                Sys.chdir tgt;
                BaseExec.run "ocaml" ["setup.ml"; "-distclean"];
-               BaseExec.run "oasis" ["setup"];
                Sys.chdir dir;
-               BaseExec.run "tar" ["czf"; tarball; tarname];
+               BaseExec.run "tar" ["czf"; tarball; topdir];
                Sys.chdir cur_pwd;
              with e ->
                Sys.chdir cur_pwd;
                raise e
            end)
-
 
   method tag ver =
     warning ~ctxt "No tag method"
@@ -166,17 +196,17 @@ let () =
       pkg.version
   in
 
-  let tarname = 
+  let topdir = 
     pkg.name^"-"^version
   in
 
   let tarball = 
-    tarname^".tar.gz"
+    Filename.concat (pwd ()) (topdir^".tar.gz")
   in
 
   let vcs = 
     let test_dir dn () = 
-      test Is_dir "_darcs"
+      test Is_dir dn
     in
       try 
         snd
@@ -184,25 +214,24 @@ let () =
              (fun (f, res) -> f ())
              [
                test_dir "_darcs", new darcs ctxt;
-(*
-               test_dir ".git",   Some Git;
-               test_dir ".svn",   Some Svn;
- *)
+               test_dir ".git",   new git ctxt;
+               test_dir ".svn",   new svn ctxt;
              ])
       with Not_found ->
         new no_vcs ctxt
   in
 
-  let () = 
-    if not vcs#check_dist then
-      exit 1;
-
     if not vcs#check_uncommited_changes then
-      exit 1;
-  in
+      begin
+        error ~ctxt "Uncommited changes";
+        exit 1
+      end;
 
     (* Create the tarball *)
-    vcs#dist tarname;
+    vcs#dist topdir tarball;
+
+    (* Run "oasis setup" *)
+    update_oasis_in_tarball tarball topdir;
 
     (* Check that the tarball can build *)
     with_tmpdir 
@@ -214,12 +243,30 @@ let () =
            BaseExec.run "tar" ["xz"; "-C"; dir; "-f"; tarball];
 
            Sys.chdir dir;
-           Sys.chdir tarname;
+           Sys.chdir topdir;
 
            try 
-             (* Check that build, test, doc run smoothly *)
-             BaseExec.run "ocaml" ["setup.ml"; "-all"];
-             Sys.chdir pwd
+             let () = 
+               (* Check that build, test, doc run smoothly *)
+               BaseExec.run "ocaml" ["setup.ml"; "-all"]
+             in
+
+             let () = 
+               let bak_files = 
+                 (* Check for remaining .bak files *)
+                 find (Has_extension "bak") 
+                   FilePath.current_dir
+                   (fun acc fn -> fn :: acc)
+                   []
+               in
+                 if bak_files <> [] then
+                   failwithf
+                     "Remaining .bak files: %s"
+                     (String.concat ", " bak_files)
+             in
+
+               Sys.chdir pwd
+
            with e ->
              Sys.chdir pwd;
              raise e);
