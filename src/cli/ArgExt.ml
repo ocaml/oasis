1759008edc9bffd8c7260031c07c87d0722bf66f
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2011-08-29 13:44:52 +0100

    Print version when requesting help.
    Ignore-this: 8e3244c1184c52a19575ce0a6e2270a0
    
    darcs-hash:20110829124452-6f87a-c71807c23be869127a7808840bfa1bf970e31a34

--- src/cli/ArgExt.ml
+++ src/cli/ArgExt.ml
@@ -19,7 +19,7 @@
 (*  Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA               *)
 (********************************************************************************)
 
-(** Parsing of command line arguments 
+(** Parsing of command line arguments
   *)
 
 open OASISGettext
@@ -30,34 +30,35 @@ open Format
 open FormatExt
 open FormatMarkdown
 
-let specs, usage_msg = 
+let specs, usage_msg =
   [
     "-C",
     Arg.String (fun str -> Sys.chdir str),
     (s_ "dir Change directory before running.");
-  ] 
+  ]
   @ (BaseContext.args ()),
 
-  s_ "OASIS (C) 2009-2010 OCamlCore SARL\n\
-      \n\
+  ("OASIS v" ^ OASISVersion.string_of_version OASISConf.version_full
+   ^ " (C) 2009-2010 OCamlCore SARL\n"
+   ^ s_ "\n\
       oasis [global-options*] subcommand [subcommand-options*]\n\
       \n\
-      Global options:"
+      Global options:")
 
 type help_extent =
   | NoSubCommand
   | SubCommand of string
-  | AllSubCommand 
+  | AllSubCommand
 
 type help_style =
-  | Markdown 
+  | Markdown
   | Output
 
-let pp_print_help hext hsty fmt () = 
+let pp_print_help hext hsty fmt () =
 
   (* Print with a precise length *)
   let pp_print_justified sz fmt str =
-    let ns = 
+    let ns =
       String.make sz ' '
     in
       String.blit str 0 ns 0 (String.length str);
@@ -65,9 +66,9 @@ let pp_print_help hext hsty fmt () =
   in
 
   (* Print definition for the output style *)
-  let pp_print_output_def sz fmt (term, def) = 
+  let pp_print_output_def sz fmt (term, def) =
     pp_print_string fmt "  ";
-    pp_print_justified sz fmt term; 
+    pp_print_justified sz fmt term;
     pp_print_string fmt "  ";
     pp_open_box fmt 0;
     pp_print_string_spaced fmt def;
@@ -75,11 +76,11 @@ let pp_print_help hext hsty fmt () =
     pp_print_newline fmt ()
   in
 
-  let pp_print_specs spec_help fmt specs = 
+  let pp_print_specs spec_help fmt specs =
 
-    let help_specs = 
+    let help_specs =
       List.rev_append
-        (List.rev_map 
+        (List.rev_map
            (fun (cli, t, hlp) ->
               let arg, hlp =
                 match split ' ' hlp with
@@ -89,13 +90,13 @@ let pp_print_help hext hsty fmt () =
                       "", ""
               in
               let arg =
-                match t with 
+                match t with
                   | Arg.Symbol (lst, _) ->
                       "{"^(String.concat "|" lst)^"}"
                   | _ ->
                       arg
               in
-              let term = 
+              let term =
                 if arg <> "" then
                   cli^" "^arg
                 else
@@ -109,15 +110,15 @@ let pp_print_help hext hsty fmt () =
            [])
     in
 
-    let sz = 
+    let sz =
       List.fold_left
         (fun acc (s, _) -> max (String.length s) acc)
         0
         help_specs
     in
 
-    let pp_print_spec fmt (term, hlp) = 
-      match hsty with 
+    let pp_print_spec fmt (term, hlp) =
+      match hsty with
         | Markdown ->
             pp_print_def fmt
               ("`"^term^"`")
@@ -126,15 +127,15 @@ let pp_print_help hext hsty fmt () =
             pp_print_output_def
               sz fmt (term, hlp)
     in
-      
+
       pp_print_list pp_print_spec "" fmt help_specs;
       if hsty = Output then
         pp_print_newline fmt ()
   in
 
-  let pp_print_scmds fmt () = 
+  let pp_print_scmds fmt () =
 
-    let sz = 
+    let sz =
       SubCommand.fold
         (fun c sz ->
            max sz (String.length c.scmd_name))
@@ -144,24 +145,24 @@ let pp_print_help hext hsty fmt () =
 
       SubCommand.fold
         (fun c () ->
-           match hsty with 
+           match hsty with
              | Markdown ->
                  pp_print_def fmt ("`"^c.scmd_name^"`")
                    [pp_print_string_spaced, c.scmd_synopsis]
              | Output ->
-                 pp_print_output_def 
+                 pp_print_output_def
                    sz fmt (c.scmd_name, c.scmd_synopsis))
         ();
       if hsty = Output then
         pp_print_newline fmt ()
   in
 
-  let pp_print_scmd fmt scmd = 
-    pp_print_title 2 fmt 
+  let pp_print_scmd fmt scmd =
+    pp_print_title 2 fmt
       (Printf.sprintf (f_ "Subcommand %s") scmd.scmd_name);
-    
+
     pp_print_string fmt scmd.scmd_help;
-    pp_print_endblock 
+    pp_print_endblock
       ~check_last_char:scmd.scmd_help
       fmt ();
 
@@ -180,7 +181,7 @@ let pp_print_help hext hsty fmt () =
     pp_print_endblock fmt ();
 
     pp_print_string fmt CLIData.main_mkd;
-    pp_print_endblock 
+    pp_print_endblock
       ~check_last_char:CLIData.main_mkd
       fmt ();
 
@@ -189,11 +190,11 @@ let pp_print_help hext hsty fmt () =
     pp_print_scmds fmt ();
 
     begin
-      match hext with 
+      match hext with
         | NoSubCommand ->
             ()
         | SubCommand nm ->
-            pp_print_scmd fmt 
+            pp_print_scmd fmt
               (SubCommand.find nm)
         | AllSubCommand ->
             SubCommand.fold
@@ -203,23 +204,23 @@ let pp_print_help hext hsty fmt () =
     end
 
 
-let parse () = 
-  let pos = 
+let parse () =
+  let pos =
     ref 0
   in
 
-  let scmd = 
-    ref 
-      (SubCommand.make 
+  let scmd =
+    ref
+      (SubCommand.make
          (s_ "none")
          ""
          ""
          (fun () ->
-            error "%s" 
+            error "%s"
               (s_ "No subcommand defined, call 'oasis -help' for help")))
   in
 
-  let scmd_args = 
+  let scmd_args =
     ref [||]
   in
 
@@ -227,22 +228,22 @@ let parse () =
     scmd := SubCommand.find s;
 
     (* Get the rest of arguments *)
-    scmd_args := 
+    scmd_args :=
     Array.sub Sys.argv !pos ((Array.length Sys.argv) - !pos);
-    
+
     (* Skip arguments *)
     pos := !pos + Array.length !scmd_args
   in
 
-  let handle_error exc hext = 
-    let get_bad str = 
+  let handle_error exc hext =
+    let get_bad str =
       match split '\n' str with
         | fst :: _ ->
             fst
         | [] ->
             s_ "Unknown error on the command line"
     in
-      match exc with 
+      match exc with
         | Arg.Bad txt ->
             pp_print_help hext Output err_formatter ();
             prerr_newline ();
@@ -257,8 +258,8 @@ let parse () =
 
     (* Parse global options and set scmd *)
     begin
-      try 
-        Arg.parse_argv 
+      try
+        Arg.parse_argv
           ~current:pos
           Sys.argv
           (Arg.align specs)
