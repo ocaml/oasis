7f292175b4f5a4e43c843f59f6e72e9575d95fb3
Sylvain Le Gall <sylvain.le-gall@ocamlcore.com>
2012-03-19 23:56:27 +0000

    Update using oasis 0.3.0~rc3
    Ignore-this: 2e2597bf1fd613d068b62dc34048bc83
    
    darcs-hash:20120319235627-a43a9-016b4c4b480249cf0afc680605441e4a3e2cfc2c

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -113,7 +113,7 @@ rule "ocamlify: %.mlify & %.mlify.depends -> %.ml"
 ;;
 
 (* OASIS_START *)
-(* DO NOT EDIT (digest: b3b8b026ec8c9b66ccab364b8c080ae2) *)
+(* DO NOT EDIT (digest: 5017c065dd9ad2160184949adbb44faa) *)
 module OASISGettext = struct
 # 21 "/home/gildor/programmation/oasis/src/oasis/OASISGettext.ml"
   
@@ -444,19 +444,24 @@ module MyOCamlbuildBase = struct
   
   
   open Ocamlbuild_plugin
+  module OC = Ocamlbuild_pack.Ocaml_compiler
   
   type dir = string 
   type file = string 
   type name = string 
   type tag = string 
   
-# 55 "/home/gildor/programmation/oasis/src/plugins/ocamlbuild/MyOCamlbuildBase.ml"
+# 56 "/home/gildor/programmation/oasis/src/plugins/ocamlbuild/MyOCamlbuildBase.ml"
   
   type t =
       {
         lib_ocaml: (name * dir list) list;
         lib_c:     (name * dir * file list) list; 
         flags:     (tag list * (spec OASISExpr.choices)) list;
+        (* Replace the 'dir: include' from _tags by a precise interdepends in
+         * directory.
+         *)
+        includes:  (dir * dir list) list; 
       } 
   
   let env_filename =
@@ -468,6 +473,12 @@ module MyOCamlbuildBase = struct
       List.iter 
         (fun dispatch -> dispatch e)
         lst 
+  
+  let tag_libstubs nm =
+    "use_lib"^nm^"_stubs"
+  
+  let nm_libstubs nm =
+    nm^"_stubs"
   
   let dispatch t e = 
     let env = 
@@ -496,43 +507,123 @@ module MyOCamlbuildBase = struct
                   Options.ext_dll, "ext_dll";
                 ]
   
+        | Before_rules ->
+          (* TODO: move this into its own file and conditionnaly include it, if
+           * needed.
+           *)
+          (* OCaml cmxs rules: cmxs available in ocamlopt but not ocamlbuild.
+             Copied from ocaml_specific.ml in ocamlbuild sources. *)
+          let has_native_dynlink =
+            try
+              bool_of_string (BaseEnvLight.var_get "native_dynlink" env)
+            with Not_found ->
+              false
+          in
+          if has_native_dynlink && String.sub Sys.ocaml_version 0 4 = "3.11" then
+            begin
+              let ext_lib = !Options.ext_lib in
+              let ext_obj = !Options.ext_obj in
+              let ext_dll = !Options.ext_dll in
+              let x_o = "%"-.-ext_obj in
+              let x_a = "%"-.-ext_lib in
+              let x_dll = "%"-.-ext_dll in
+              let x_p_o = "%.p"-.-ext_obj in
+              let x_p_a = "%.p"-.-ext_lib in
+              let x_p_dll = "%.p"-.-ext_dll in
+  
+              rule "ocaml: mldylib & p.cmx* & p.o* -> p.cmxs & p.so"
+                   ~tags:["ocaml"; "native"; "profile"; "shared"; "library"]
+                   ~prods:["%.p.cmxs"; x_p_dll]
+                   ~dep:"%.mldylib"
+                   (OC.native_profile_shared_library_link_mldylib
+                      "%.mldylib" "%.p.cmxs");
+  
+              rule "ocaml: mldylib & cmx* & o* -> cmxs & so"
+                   ~tags:["ocaml"; "native"; "shared"; "library"]
+                   ~prods:["%.cmxs"; x_dll]
+                   ~dep:"%.mldylib"
+                   (OC.native_shared_library_link_mldylib
+                      "%.mldylib" "%.cmxs");
+  
+              rule "ocaml: p.cmx & p.o -> p.cmxs & p.so"
+                   ~tags:["ocaml"; "native"; "profile"; "shared"; "library"]
+                   ~prods:["%.p.cmxs"; x_p_dll]
+                   ~deps:["%.p.cmx"; x_p_o]
+                   (OC.native_shared_library_link ~tags:["profile"]
+                                                  "%.p.cmx" "%.p.cmxs");
+  
+              rule "ocaml: p.cmxa & p.a -> p.cmxs & p.so"
+                   ~tags:["ocaml"; "native"; "profile"; "shared"; "library"]
+                   ~prods:["%.p.cmxs"; x_p_dll]
+                   ~deps:["%.p.cmxa"; x_p_a]
+                   (OC.native_shared_library_link ~tags:["profile"; "linkall"]
+                                                  "%.p.cmxa" "%.p.cmxs");
+  
+              rule "ocaml: cmx & o -> cmxs"
+                   ~tags:["ocaml"; "native"; "shared"; "library"]
+                   ~prods:["%.cmxs"]
+                   ~deps:["%.cmx"; x_o]
+                   (OC.native_shared_library_link "%.cmx" "%.cmxs");
+  
+              rule "ocaml: cmx & o -> cmxs & so"
+                   ~tags:["ocaml"; "native"; "shared"; "library"]
+                   ~prods:["%.cmxs"; x_dll]
+                   ~deps:["%.cmx"; x_o]
+                   (OC.native_shared_library_link "%.cmx" "%.cmxs");
+  
+              rule "ocaml: cmxa & a -> cmxs & so"
+                   ~tags:["ocaml"; "native"; "shared"; "library"]
+                   ~prods:["%.cmxs"; x_dll]
+                   ~deps:["%.cmxa"; x_a]
+                   (OC.native_shared_library_link ~tags:["linkall"]
+                                                  "%.cmxa" "%.cmxs");
+            end
+  
         | After_rules -> 
             (* Declare OCaml libraries *)
             List.iter 
               (function
-                 | lib, [] ->
-                     ocaml_lib lib;
-                 | lib, dir :: tl ->
-                     ocaml_lib ~dir:dir lib;
+                 | nm, [] ->
+                     ocaml_lib nm
+                 | nm, dir :: tl ->
+                     ocaml_lib ~dir:dir (dir^"/"^nm);
                      List.iter 
                        (fun dir -> 
-                          flag 
-                            ["ocaml"; "use_"^lib; "compile"] 
-                            (S[A"-I"; P dir]))
+                          List.iter
+                            (fun str ->
+                               flag ["ocaml"; "use_"^nm; str] (S[A"-I"; P dir]))
+                            ["compile"; "infer_interface"; "doc"])
                        tl)
               t.lib_ocaml;
+  
+            (* Declare directories dependencies, replace "include" in _tags. *)
+            List.iter 
+              (fun (dir, include_dirs) ->
+                 Pathname.define_context dir include_dirs)
+              t.includes;
   
             (* Declare C libraries *)
             List.iter
               (fun (lib, dir, headers) ->
                    (* Handle C part of library *)
-                   flag ["link"; "library"; "ocaml"; "byte"; "use_lib"^lib]
-                     (S[A"-dllib"; A("-l"^lib); A"-cclib"; A("-l"^lib)]);
+                   flag ["link"; "library"; "ocaml"; "byte"; tag_libstubs lib]
+                     (S[A"-dllib"; A("-l"^(nm_libstubs lib)); A"-cclib";
+                        A("-l"^(nm_libstubs lib))]);
   
-                   flag ["link"; "library"; "ocaml"; "native"; "use_lib"^lib]
-                     (S[A"-cclib"; A("-l"^lib)]);
+                   flag ["link"; "library"; "ocaml"; "native"; tag_libstubs lib]
+                     (S[A"-cclib"; A("-l"^(nm_libstubs lib))]);
                         
-                   flag ["link"; "program"; "ocaml"; "byte"; "use_lib"^lib]
-                     (S[A"-dllib"; A("dll"^lib)]);
+                   flag ["link"; "program"; "ocaml"; "byte"; tag_libstubs lib]
+                     (S[A"-dllib"; A("dll"^(nm_libstubs lib))]);
   
                    (* When ocaml link something that use the C library, then one
                       need that file to be up to date.
                     *)
-                   dep  ["link"; "ocaml"; "program"; "use_lib"^lib]
-                     [dir/"lib"^lib^"."^(!Options.ext_lib)];
+                   dep  ["link"; "ocaml"; "program"; tag_libstubs lib]
+                     [dir/"lib"^(nm_libstubs lib)^"."^(!Options.ext_lib)];
   
-                   dep  ["compile"; "ocaml"; "program"; "use_lib"^lib]
-                     [dir/"lib"^lib^"."^(!Options.ext_lib)];
+                   dep  ["compile"; "ocaml"; "program"; tag_libstubs lib]
+                     [dir/"lib"^(nm_libstubs lib)^"."^(!Options.ext_lib)];
   
                    (* TODO: be more specific about what depends on headers *)
                    (* Depends on .h files *)
@@ -571,19 +662,121 @@ let package_default =
   {
      MyOCamlbuildBase.lib_ocaml =
        [
-          ("src/oasis/oasis", ["src/oasis"]);
-          ("src/base/base", ["src/base"]);
-          ("src/builtin-plugins", ["src"]);
-          ("src/cli/cli", ["src/cli"])
+          ("oasis", ["src/oasis"]);
+          ("base", ["src/base"]);
+          ("builtin-plugins", ["src"]);
+          ("cli", ["src/cli"])
        ];
      lib_c = [];
      flags = [];
+     includes =
+       [
+          ("test", ["src"; "src/base"; "src/oasis"]);
+          ("src/plugins/ocamlbuild",
+            [
+               "src";
+               "src/base";
+               "src/oasis";
+               "src/plugins/custom";
+               "src/plugins/extra/META";
+               "src/plugins/extra/devfiles";
+               "src/plugins/extra/stdfiles";
+               "src/plugins/internal";
+               "src/plugins/none"
+            ]);
+          ("src/plugins/none",
+            [
+               "src";
+               "src/base";
+               "src/oasis";
+               "src/plugins/custom";
+               "src/plugins/extra/META";
+               "src/plugins/extra/devfiles";
+               "src/plugins/extra/stdfiles";
+               "src/plugins/internal";
+               "src/plugins/ocamlbuild"
+            ]);
+          ("src/plugins/internal",
+            [
+               "src";
+               "src/base";
+               "src/oasis";
+               "src/plugins/custom";
+               "src/plugins/extra/META";
+               "src/plugins/extra/devfiles";
+               "src/plugins/extra/stdfiles";
+               "src/plugins/none";
+               "src/plugins/ocamlbuild"
+            ]);
+          ("src/plugins/extra/stdfiles",
+            [
+               "src";
+               "src/base";
+               "src/oasis";
+               "src/plugins/custom";
+               "src/plugins/extra/META";
+               "src/plugins/extra/devfiles";
+               "src/plugins/internal";
+               "src/plugins/none";
+               "src/plugins/ocamlbuild"
+            ]);
+          ("src/plugins/extra/devfiles",
+            [
+               "src";
+               "src/base";
+               "src/oasis";
+               "src/plugins/custom";
+               "src/plugins/extra/META";
+               "src/plugins/extra/stdfiles";
+               "src/plugins/internal";
+               "src/plugins/none";
+               "src/plugins/ocamlbuild"
+            ]);
+          ("src/plugins/extra/META",
+            [
+               "src";
+               "src/base";
+               "src/oasis";
+               "src/plugins/custom";
+               "src/plugins/extra/devfiles";
+               "src/plugins/extra/stdfiles";
+               "src/plugins/internal";
+               "src/plugins/none";
+               "src/plugins/ocamlbuild"
+            ]);
+          ("src/plugins/custom",
+            [
+               "src";
+               "src/base";
+               "src/oasis";
+               "src/plugins/extra/META";
+               "src/plugins/extra/devfiles";
+               "src/plugins/extra/stdfiles";
+               "src/plugins/internal";
+               "src/plugins/none";
+               "src/plugins/ocamlbuild"
+            ]);
+          ("src/cli", ["src"; "src/base"; "src/oasis"]);
+          ("src/base", ["src/oasis"]);
+          ("src",
+            [
+               "src/base";
+               "src/oasis";
+               "src/plugins/custom";
+               "src/plugins/extra/META";
+               "src/plugins/extra/devfiles";
+               "src/plugins/extra/stdfiles";
+               "src/plugins/internal";
+               "src/plugins/none";
+               "src/plugins/ocamlbuild"
+            ])
+       ];
      }
   ;;
 
 let dispatch_default = MyOCamlbuildBase.dispatch_default package_default;;
 
-# 473 "myocamlbuild.ml"
+# 666 "myocamlbuild.ml"
 (* OASIS_STOP *)
 
 open Ocamlbuild_plugin;;
