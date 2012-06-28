feea043930f5c9229216a9747b87537d46dc14d5
Sylvain Le Gall <sylvain.le-gall@ocamlcore.com>
2012-06-28 02:36:29 +0100

    Add continuous integration scripts
    Ignore-this: dec9d08b091b35f275b5f675e255ceb0
    
    darcs-hash:20120628013629-a43a9-7f13654ec7ab2e5d51c8247d7f96b6984be3bc9a

---
+++ ci-main.lua
@@ -0,0 +1,28 @@
+bootstrap = require("bootstrap")
+
+bootstrap.init()
+
+ci = require("ci")
+godi = require("godi")
+oasis = require("oasis")
+darcs = require("darcs")
+
+ci.init()
+godi.init()
+darcs.init()
+oasis.init()
+
+godi.bootstrap("3.12")
+godi.update()
+godi.upgrade()
+godi.build_many(
+  {"godi-findlib",
+   "godi-ocaml-fileutils",
+   "godi-ocaml-data-notation",
+   "godi-expect",
+   "godi-ounit",
+   "apps-ocamlmod",
+   "apps-ocamlify"})
+
+oasis.std_process("--enable-tests")
+darcs.create_tag(oasis.package_version())
