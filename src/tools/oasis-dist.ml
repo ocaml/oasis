f0feef5cedfe29fabeb80b9a36e756e33e8138bb
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2013-02-16 12:22:52 +0100

    Locate "topfind" when using opam "system" compiler.
    When using opam with the "system" compiler the "topfind" file is not
    found by the toplevel.  However, the environment variable
    $OCAML_TOPLEVEL_PATH is set. See
    https://github.com/OCamlPro/opam-repository/issues/34

--- src/tools/oasis-dist.ml
+++ src/tools/oasis-dist.ml
@@ -21,6 +21,10 @@
 (* Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA              *)
 (******************************************************************************)
 
+let () =
+  try Topdirs.dir_directory (Sys.getenv "OCAML_TOPLEVEL_PATH")
+  with Not_found -> ();;
+
 #use "topfind";;
 #require "oasis";;
 #require "oasis.base";;
