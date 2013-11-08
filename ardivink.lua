162827e0b066a881c7b59fb1cedd572d43c24444
Sylvain Le Gall <gildor@debian.org>
2013-11-08 23:01:53 +0100

    Enable JUnit output.

--- ardivink.lua
+++ ardivink.lua
@@ -6,8 +6,10 @@ ci.init()
 -- godi.init()
 ci.prependenv("PATH", "/usr/opt/godi/bin")
 ci.prependenv("PATH", "/usr/opt/godi/sbin")
+ci.putenv("OUNIT_OUTPUT_HTML_DIR", dist.make_filename("ounit-log-$(suite_name).html"))
+ci.putenv("OUNIT_OUTPUT_JUNIT_FILE", dist.make_filename("junit-$(suite_name).xml"))
+ci.putenv("OUNIT_OUTPUT_FILE", dist.make_filename("ounit-log-$(suite_name)-$(shard_id).txt"))
 
-git.init()
 oasis.init()
 
 oasis.std_process("--enable-tests")
