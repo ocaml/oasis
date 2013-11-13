c6040ef9786e302279bba5ed13fc84f1fd77e419
Sylvain Le Gall <gildor@debian.org>
2013-11-13 23:06:29 +0100

    Fix broken build due to missing dist/.

--- ardivink.lua
+++ ardivink.lua
@@ -1,16 +1,16 @@
 ci = require("ci")
+dist = require("dist")
 oasis = require("oasis")
-git = require("git")
 
 ci.init()
--- godi.init()
+dist.init()
+oasis.init()
+
 ci.prependenv("PATH", "/usr/opt/godi/bin")
 ci.prependenv("PATH", "/usr/opt/godi/sbin")
 ci.putenv("OUNIT_OUTPUT_HTML_DIR", dist.make_filename("ounit-log-$(suite_name).html"))
 ci.putenv("OUNIT_OUTPUT_JUNIT_FILE", dist.make_filename("junit-$(suite_name).xml"))
 ci.putenv("OUNIT_OUTPUT_FILE", dist.make_filename("ounit-log-$(suite_name)-$(shard_id).txt"))
-
-oasis.init()
 
 oasis.std_process("--enable-tests")
 
