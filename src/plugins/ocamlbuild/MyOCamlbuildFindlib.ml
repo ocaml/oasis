f1d24e776cb698f3bd8418b82a2f6a9b7d83e1ec
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2014-05-07 18:55:35 +0200

    Use pflag instead of flag

--- src/plugins/ocamlbuild/MyOCamlbuildFindlib.ml
+++ src/plugins/ocamlbuild/MyOCamlbuildFindlib.ml
@@ -133,34 +133,42 @@ let dispatch =
         (* For each ocamlfind package one inject the -package option when
          * compiling, computing dependencies, generating documentation and
          * linking. *)
+        (* TODO: consider how to really choose camlp4o or camlp4r. *)
+        let syn_args = [A"-syntax"; A "camlp4o"] in
+        let is_syntax pkg =
+          (* Heuristic to identify syntax extensions: whether they end in
+             ".syntax"; some might not.
+          *)
+          Filename.check_suffix pkg "syntax" ||
+          List.mem pkg well_known_syntax
+        in
         List.iter
           begin fun pkg ->
             let base_args = [A"-package"; A pkg] in
-            (* TODO: consider how to really choose camlp4o or camlp4r. *)
-            let syn_args = [A"-syntax"; A "camlp4o"] in
-            let (args, pargs) =
-            (* Heuristic to identify syntax extensions: whether they end in
-               ".syntax"; some might not.
-             *)
-              if Filename.check_suffix pkg "syntax" ||
-                 List.mem pkg well_known_syntax then
-                (syn_args @ base_args, syn_args)
+            let args =
+              if is_syntax pkg then
+                syn_args @ base_args
               else
-                (base_args, [])
+                base_args
             in
             flag ["ocaml"; "compile";  "pkg_"^pkg] & S args;
             flag ["ocaml"; "ocamldep"; "pkg_"^pkg] & S args;
             flag ["ocaml"; "doc";      "pkg_"^pkg] & S args;
             flag ["ocaml"; "link";     "pkg_"^pkg] & S base_args;
             flag ["ocaml"; "infer_interface"; "pkg_"^pkg] & S args;
-
-            (* TODO: Check if this is allowed for OCaml < 3.12.1 *)
-            flag ["ocaml"; "compile";  "package("^pkg^")"] & S pargs;
-            flag ["ocaml"; "ocamldep"; "package("^pkg^")"] & S pargs;
-            flag ["ocaml"; "doc";      "package("^pkg^")"] & S pargs;
-            flag ["ocaml"; "infer_interface"; "package("^pkg^")"] & S pargs;
           end
           (find_packages ());
+
+        let get_syn_args pkg =
+          if is_syntax pkg then
+            S syn_args
+          else
+            N
+        in
+        pflag ["ocaml"; "compile"] "package" get_syn_args;
+        pflag ["ocaml"; "ocamldep"] "package" get_syn_args;
+        pflag ["ocaml"; "doc"] "package" get_syn_args;
+        pflag ["ocaml"; "infer_interface"] "package" get_syn_args;
 
         (* Like -package but for extensions syntax. Morover -syntax is useless
          * when linking. *)
