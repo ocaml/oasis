343c01f8c82cd65fc31e13521453274f6df6c403
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2015-11-22 00:03:02 +0100

    Appveyor: add a test for dynamic mode

--- appveyor_build.sh
+++ appveyor_build.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
+function run {
+    NAME=$1
+    shift
+    echo -n "$NAME ... "
+    $@
+    CODE=$?
+    if [ $CODE -ne 0 ]; then
+        echo "... $NAME failed!"
+        exit $CODE
+    else
+        echo "... $NAME OK"
+    fi
+}
+
 cd "$APPVEYOR_BUILD_FOLDER"
 
-echo "Initialize OPAM..."
-opam init -y -a
-opam install -y ocamlfind ocaml-data-notation ocamlmod ocamlify
+run "OPAM initialization" opam init -y -a
+run "Install packages" \
+    opam install -y ocamlfind ocaml-data-notation ocamlmod ocamlify
 eval $(opam config env)
 export OCAML_TOPLEVEL_PATH=$(opam config var toplevel)
 
-echo "Compile OASIS..."
-ocaml setup.ml -configure
-ocaml setup.ml -build
+run "OASIS Configure step" ocaml setup.ml -configure
+run "OASIS Build step" ocaml setup.ml -build
 
+run "Install" ocaml setup.ml -install
+
+echo "------------------------------------------------------------"
+echo "Rebuild with dynamic mode"
+run "Setup" ./Main.native setup -setup-update dynamic
+run "Configure" ocaml setup.ml -info -configure
+run "Build" ocaml setup.ml -build
