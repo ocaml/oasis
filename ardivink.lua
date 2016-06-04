97b70ad8d2830356f4eefe72e4eabbb93400ddf8
Sylvain Le Gall <sylvain@le-gall.net>
2016-06-04 18:30:29 +0200

    Enable devel mode for ardivink.

--- ardivink.lua
+++ ardivink.lua
@@ -12,7 +12,7 @@ ci.putenv("OUNIT_OUTPUT_HTML_DIR", dist.make_filename("ounit-log-$(suite_name).h
 ci.putenv("OUNIT_OUTPUT_JUNIT_FILE", dist.make_filename("junit-$(suite_name).xml"))
 ci.putenv("OUNIT_OUTPUT_FILE", dist.make_filename("ounit-log-$(suite_name)-$(shard_id).txt"))
 
-oasis.std_process("--enable-tests")
+oasis.std_process("--enable-tests", "--enable-devel")
 
 -- Create documentation package.
 ci.exec("make", "doc-dist")
