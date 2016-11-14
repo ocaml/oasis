2909268d45a4402c7333edf42e0065dcd22f8aca
Sylvain Le Gall <sylvain@le-gall.net>
2016-11-14 01:46:01 +0100

    Only change directory to 'dist' for running opam-build-revdeps.

--- src/tools/ci/opam-build-revdeps.bash
+++ src/tools/ci/opam-build-revdeps.bash
@@ -1,10 +1,10 @@
 mkdir dist || true
 mkdir tmp || true
-cd dist
 . "$(dirname $0)/opam.bash" || exit 1
 # TODO: use the published version of opam-build-revdeps
 opam pin opam-build-revdeps \
   git://github.com/gildor478/opam-build-revdeps.git#opam/unstable
+cd dist
 opam-build-revdeps compare --package oasis \
   --version1 latest \
   --version2 latest -pin2 "oasis:$(pwd)/.." \
