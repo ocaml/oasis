b9a871d21954726bc1ddf34e3d9664b64c7acd5a
Sylvain Le Gall <sylvain@le-gall.net>
2016-11-15 00:41:25 +0100

    Allow to switch to opam/unstable for opam-build-revdeps with environment variable.

--- src/tools/ci/opam-build-revdeps.bash
+++ src/tools/ci/opam-build-revdeps.bash
@@ -1,9 +1,17 @@
 mkdir dist || true
 mkdir tmp || true
 . "$(dirname $0)/opam.bash" || exit 1
-# TODO: use the published version of opam-build-revdeps
-opam pin add opam-build-revdeps \
-  git://github.com/gildor478/opam-build-revdeps.git#opam/unstable
+
+env
+
+OPAM_BUILD_REVDEPS_UNSTABLE="no"
+if [ "x${OPAM_BUILD_REVDEPS_UNSTABLE}" == "xyes" ] ; then
+  opam pin add opam-build-revdeps \
+    'git://github.com/gildor478/opam-build-revdeps.git#opam/unstable'
+else
+  opan install opam-build-revdeps
+fi
+
 # TODO: un-exclude when upgrading to Debian Stretch
 # - maxminddb
 # - brotli
@@ -18,5 +26,6 @@ opam-build-revdeps compare --package oasis \
   --ocaml_version 4.03.0 \
   --version1 latest \
   --version2 latest --pin2 "oasis:$(pwd)/sut" \
-  --html_output "dist/index.html"
+  --html_output "dist/index.html" \
+  --css_output "dist/index.css"
 
