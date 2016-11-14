dd41a49e067cea1da4e58d712c23f272dbe1456d
Sylvain Le Gall <sylvain@le-gall.net>
2016-11-14 09:35:49 +0100

    Use a subdirectory 'sut' to checkout branch opam/unstable.

--- src/tools/ci/opam-build-revdeps.bash
+++ src/tools/ci/opam-build-revdeps.bash
@@ -4,9 +4,8 @@ mkdir tmp || true
 # TODO: use the published version of opam-build-revdeps
 opam pin add opam-build-revdeps \
   git://github.com/gildor478/opam-build-revdeps.git#opam/unstable
-cd dist
 opam-build-revdeps compare --package oasis \
   --version1 latest \
-  --version2 latest --pin2 "oasis:$(pwd)/.." \
-  --root_dir "$(pwd)/../tmp"
+  --version2 latest --pin2 "oasis:$(pwd)/sut" \
+  --html_output "dist/output.html"
 
