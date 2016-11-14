b489b25b3b449fbc46d083d77d5f079989439ddc
Sylvain Le Gall <sylvain@le-gall.net>
2016-11-14 07:58:00 +0100

    Fix typo when calling opam-build-revdeps.

--- src/tools/ci/opam-build-revdeps.bash
+++ src/tools/ci/opam-build-revdeps.bash
@@ -7,6 +7,6 @@ opam pin add opam-build-revdeps \
 cd dist
 opam-build-revdeps compare --package oasis \
   --version1 latest \
-  --version2 latest -pin2 "oasis:$(pwd)/.." \
+  --version2 latest --pin2 "oasis:$(pwd)/.." \
   --root_dir "$(pwd)/../tmp"
 
