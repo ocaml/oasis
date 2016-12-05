4a6be1f06a99178d00ce560b5c7ffbc9f2c612b5
Sylvain Le Gall <sylvain@le-gall.net>
2016-12-06 00:53:42 +0100

    Try to forece omake.0.9.8 even after an upgrade for CI tests (2nd).

--- src/tools/ci/build.bash
+++ src/tools/ci/build.bash
@@ -1,8 +1,8 @@
 . "$(dirname $0)/packages.bash" || exit 1
 . "$(dirname $0)/opam.bash" || exit 1
 mkdir dist || true
-opam install $OPAM_PKGS
 opam install omake.0.9.8.6-0.rc1
+opam install $OPAM_PKGS
 export OCAMLRUNPARAM=b
 ocaml setup.ml -distclean
 ocaml setup.ml -configure --enable-tests --enable-devel ${CONFIGURE_ARGS}
