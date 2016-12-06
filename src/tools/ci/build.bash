1ca286d5754df54c5d3ccb9cfe8d90865ae9461c
Sylvain Le Gall <sylvain@le-gall.net>
2016-12-06 07:38:27 +0100

    Use a bash array for OPAM_PKGS.

--- src/tools/ci/build.bash
+++ src/tools/ci/build.bash
@@ -1,7 +1,7 @@
 . "$(dirname $0)/packages.bash" || exit 1
 . "$(dirname $0)/opam.bash" || exit 1
 mkdir dist || true
-opam install $OPAM_PKGS
+opam install "${OPAM_PKGS[@]}"
 export OCAMLRUNPARAM=b
 ocaml setup.ml -distclean
 ocaml setup.ml -configure --enable-tests --enable-devel ${CONFIGURE_ARGS}
--- src/tools/ci/packages.bash
+++ src/tools/ci/packages.bash
@@ -1,12 +1,13 @@
-OPAM_PKGS="base-bytes
- base-unix
- 'ocamlfind>=1.3.1'
- pcre
- 'expect>=0.0.4'
- 'fileutils>=0.4.2'
- 'ounit>=2.0.0'
- ocamlify
- ocamlmod
- 'omake.0.9.8.6-0.rc1'
- 'benchmark>=1.2'
- camlp4"
+OPAM_PKGS=()
+OPAM_PKGS+=( "base-bytes")
+OPAM_PKGS+=( "base-unix" )
+OPAM_PKGS+=( "ocamlfind>=1.3.1" )
+OPAM_PKGS+=( "pcre" )
+OPAM_PKGS+=( "expect>=0.0.4" )
+OPAM_PKGS+=( "fileutils>=0.4.2" )
+OPAM_PKGS+=( "ounit>=2.0.0" )
+OPAM_PKGS+=( "ocamlify" )
+OPAM_PKGS+=( "ocamlmod" )
+OPAM_PKGS+=( "omake.0.9.8.6-0.rc1" )
+OPAM_PKGS+=( "benchmark>=1.2" )
+OPAM_PKGS+=( "camlp4" )
