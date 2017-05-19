3e9199ea225fef649025b8232c424b2ee5ba846c
Sylvain Le Gall <sylvain@le-gall.net>
2017-05-20 01:08:52 +0200

    Update version to 0.4.10~HEAD.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -107,7 +107,7 @@ rule "ocamlify: %.mlify & %.mlify.depends -> %.ml"
 ;;
 
 (* OASIS_START *)
-(* DO NOT EDIT (digest: 814f32d1cfa71aa70ce3b3ea4b1bbbe1) *)
+(* DO NOT EDIT (digest: f1d8705831b063bbf5776cfd09dd68b3) *)
 module OASISGettext = struct
 # 22 "src/oasis/OASISGettext.ml"
 
@@ -213,7 +213,10 @@ module OASISString = struct
         ok := false;
       incr str_idx
     done;
-    !what_idx = String.length what
+    if !what_idx = String.length what then
+      true
+    else
+      false
 
 
   let strip_starts_with ~what str =
@@ -236,7 +239,10 @@ module OASISString = struct
         ok := false;
       decr str_idx
     done;
-    !what_idx = -1
+    if !what_idx = -1 then
+      true
+    else
+      false
 
 
   let strip_ends_with ~what str =
@@ -542,7 +548,7 @@ module OASISExpr = struct
 end
 
 
-# 437 "myocamlbuild.ml"
+# 443 "myocamlbuild.ml"
 module BaseEnvLight = struct
 # 22 "src/base/BaseEnvLight.ml"
 
@@ -622,7 +628,7 @@ module BaseEnvLight = struct
 end
 
 
-# 517 "myocamlbuild.ml"
+# 523 "myocamlbuild.ml"
 module MyOCamlbuildFindlib = struct
 # 22 "src/plugins/ocamlbuild/MyOCamlbuildFindlib.ml"
 
@@ -983,7 +989,7 @@ module MyOCamlbuildBase = struct
 end
 
 
-# 878 "myocamlbuild.ml"
+# 884 "myocamlbuild.ml"
 open Ocamlbuild_plugin;;
 let package_default =
   {
@@ -1282,7 +1288,7 @@ let conf = {MyOCamlbuildFindlib.no_automatic_syntax = false}
 
 let dispatch_default = MyOCamlbuildBase.dispatch_default conf package_default;;
 
-# 1178 "myocamlbuild.ml"
+# 1184 "myocamlbuild.ml"
 (* OASIS_STOP *)
 
 open Ocamlbuild_plugin;;
